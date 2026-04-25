8fb2ac3b0b61add18e32b5bb001475095f01d1e4
Luca Saladino <sal65535@protonmail.com>
2026-04-25 12:45:41 +0200

    Implemented registry

---
+++ command/registry.go
@@ -0,0 +1,37 @@
+package command
+
+import (
+	"fmt"
+)
+
+type Registry struct {
+	commands map[string]Interface
+}
+
+func NewCommands() *Registry {
+    return &Registry{
+		commands: make(map[string]Interface),
+	}
+}
+
+func (r *Registry) Register(s Interface) {
+    r.commands[s.GetHeader().Use] = s
+}
+
+func (r *Registry) Get(name string) (Interface, error) {
+    s, ok := r.commands[name]
+    if !ok {
+        return nil, fmt.Errorf("command %q not found", name)
+    }
+
+    return s, nil
+}
+
+func (r *Registry) All() []Interface {
+    out := make([]Interface, 0, len(r.commands))
+    for _, s := range r.commands {
+        out = append(out, s)
+    }
+
+    return out
+}
